5ee7b24847496e36a222c6995d69dad86c7c7981
jiahuang <jialiya.huang@students.olin.edu>
2012-12-03 03:34:27 -0500

    bumped receiving byte size up to 8

--- ant/src/nRF24L01-rx.c
+++ ant/src/nRF24L01-rx.c
@@ -28,14 +28,14 @@ void init_nRF_pins(void)
 
 //Reads the current RX buffer into the data array
 //Forces an RX buffer flush
-void receive_data(void)
+void receive_data(uint8_t * data)
 {
   cbi(PORTB, TX_CSN); //Stand by mode
     tx_spi_byte(0x61); //Read RX Payload
-  data_array[0] = tx_spi_byte(0xFF);
-  data_array[1] = tx_spi_byte(0xFF);
-  data_array[2] = tx_spi_byte(0xFF);
-  data_array[3] = tx_spi_byte(0xFF);
+  data[0] = tx_spi_byte(0xFF);
+  data[1] = tx_spi_byte(0xFF);
+  data[2] = tx_spi_byte(0xFF);
+  data[3] = tx_spi_byte(0xFF);
   sbi(PORTB, TX_CSN);
   
   tx_send_byte(0xE2); //Flush RX FIFO
@@ -59,7 +59,7 @@ void configure_receiver(uint8_t * address)
 
   tx_send_command(0x26, 0x07); //Air data rate 1Mbit, 0dBm, Setup LNA
 
-  tx_send_command(0x31, 0x04); //4 byte receive payload
+  tx_send_command(0x31, 0x08); //4 byte receive payload
 
   tx_send_command(0x25, 0x02); //RF Channel 2 (default, not really needed)
 
@@ -73,7 +73,6 @@ void configure_receiver(uint8_t * address)
 
   sbi(PORTB, TX_CE); //Start receiving!
   
-  delay_us(500);
 }    
 
 //Sends the 4 bytes of payload
