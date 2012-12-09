8c8e9bb44803cecd27716a8a4584631a9aa020f6
johnnyman727 <jonathan.mckay@students.olin.edu>
2012-12-08 23:09:48 -0500

    Added watchdog timer to reset

--- ant/src/ant.c
+++ ant/src/ant.c
@@ -18,6 +18,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <avr/wdt.h>
 #include "id.c"
 
 #define sbi(var, mask)   ((var) |= (uint8_t)(1 << mask))
@@ -43,13 +44,14 @@
 // The pin register associated with the nRF24 IRQ pin
 #define NRF_IRQ 0
 
+#define MINUTES_TO_RESET 5;
+
 //Define functions
 //======================
 void ioinit(void);      //Initializes IO
 void delay_ms(uint16_t x); //General purpose delay
 void delay_us(uint8_t x);
 uint8_t ping_pong(void);
-
 // TX address. RX address needs to be the same as this
 // goes from least significant to most significant
 uint8_t data_pipe[5] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7};
@@ -60,12 +62,16 @@ uint8_t data_received[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 // The UUID itself. It can be loaded with load_UUID()
 char UUID[UUID_SIZE] = { ID_1, ID_2, ID_3, ID_4 } ;
 
+// ~8 resets per minute to get total number of resets
+uint32_t resets_remaining = 8 * MINUTES_TO_RESET;
+
 #include "nRF24L01-tx.c"
 #include "nRF24L01-rx.c"
 
 //======================
 ISR(PCINT1_vect)
 {
+  cli();
   // wake up from sleep mode
   // 1) read payload through SPI
   while( ping_pong() & 0x40) {
@@ -74,7 +80,29 @@ ISR(PCINT1_vect)
     // 3) read FIFO_STATUS to check if there are more payloads available in RX FIFO
     // 4) if there are more data in RX FIFO, repeat from step 1)
   }
+  sei();
 }
+
+ISR(WDT_vect) {
+
+    cli();
+    PORTA = PORTA ^ (1<<2 ); // flash an LED for two seconds
+
+    if (--resets_remaining <= 0) {
+
+      // Set the watchdog to stop interrupting 
+      WDTCSR |= ~(1 << WDIE);
+
+      //prescaler set to 1
+      WDTCSR &= (1 << WDCE) | ~(1 << WDP3) | (1<<WDP2 ) | (1 << WDP1);
+
+      // Enable the reset
+      WDTCSR |= (1 << WDE);
+
+    }
+    sei();
+}
+
 
 uint8_t ping_pong(void) 
 {
@@ -105,21 +133,17 @@ uint8_t ping_pong(void)
       data_array[7] = UUID[3];
 
       delay_ms((UUID[0] * 1000) & 255);
-      
+
       // Send over theping
       transmit_data(data_array);
 
-      // // wait for transmitting to be done
-      delay_ms(10);
+      PORTA = PORTA ^ (1<<LED ); // flash an LED
 
-      // delay some more for randomness
-      delay_ms(US_DELAY);
+      // wait for transmitting to be done
+      delay_ms(10);
       // go back to receiving
       configure_receiver(data_pipe);
 
-      PORTA = PORTA ^ (1<<LED ); // flash an LED
-
-      // delay for some fun
       delay_ms(20);
     }
   }
@@ -136,18 +160,16 @@ int main (void)
   // Turn off the led when it's loaded
   PORTA = PORTA & ~(1 << LED);
 
-
-
   ACSR = (1<<ACD); //Turn off Analog Comparator - this removes about 1uA
+
   PRR = 0x0B; //Reduce everything except timer0
     
   while(1)
   {
     ping_pong();
-       PORTA = PORTA ^(1 << 2);
+
    sleep_cpu(); //Sleep until a ping wakes us up on interrupt
    // sleep_disable();  
-      PORTA = PORTA ^(1 << 3);
   }
   
   return(0);
@@ -155,18 +177,24 @@ int main (void)
 
 void ioinit (void)
 {
+  cli();
   //1 = Output, 0 = Input
   DDRA = 0xFF & ~(1<<TX_MISO);//| 1<<BUTTON0 | 1<<BUTTON1 | 1<<BUTTON2 | 1<<BUTTON3 | 1<<BUTTON4);
   DDRB = 0b00000110; //(CE on PB1) (CS on PB2)
 
-  PORTA = 0b10011110;
+  PORTA = 0b10011010;
+
+  /* Clear WDRF in MCUSR */
+  MCUSR = 0x00;
+  /* Write logical one to WDCE and WDE */ 
+  WDTCSR |= (1 << WDCE) | (1 << WDE);
+  /* Turn off WDT */
+  WDTCSR = 0x00;
 
   cbi(PORTB, TX_CE); //Stand by mode
   
   //Init Timer0 for delay_us
   TCCR0B = (1<<CS00); //Set Prescaler to No Prescaling (assume we are running at internal 1MHz). CS00=1 
-
-  // configure_transmitter(data_pipe);
 
   configure_receiver(data_pipe);
 
@@ -174,6 +202,12 @@ void ioinit (void)
   GIMSK = (1 << PCIE1); // Enable pin change interrupt request on PCINT 11:8
   PCMSK1 = (1 << NRF_IRQ); 
   MCUCR = (1<<SM1)|(1<<SE); //Setup Power-down mode and enable sleep
+
+  //prescaler set to 2 seconds
+  WDTCSR |= (1 << WDCE) | (1 << WDP2) | (1 << WDP1) | (1 << WDP0);
+
+  // Set the watchdog to interrupt instead of reset
+  WDTCSR |= (1 << WDIE);
   
   sei(); //Enable interrupts
 }
