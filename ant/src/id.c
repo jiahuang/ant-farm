c27b7b423de9368b662380f5fd9662ad40232a02
johnnyman727 <jonathan.mckay@students.olin.edu>
2012-12-13 17:08:08 -0500

    Added post to queen_listener

--- ant/src/id.c
+++ ant/src/id.c
@@ -3,8 +3,8 @@
   * DO NOT TOUCH.
   */
 
-#define ID_1 0x35
+#define ID_1 0x1A
 #define ID_2 0x00
 #define ID_3 0x0A
 #define ID_4 0x00
-#define MS_DELAY 0X5B
+#define MS_DELAY 58
--- queen/queen_listener.py
+++ queen/queen_listener.py
@@ -4,10 +4,10 @@ import serial
 import os
 import subprocess
 
-arduino = serial.Serial('/dev/tty.usbmodem1421', 57600) # serial port# and baud rate
+arduino = serial.Serial('/dev/tty.usbmodemfa131', 57600) # serial port# and baud rate
 
-def post_to_server(queen_id, ant_id):
-  subprocess.call(["curl", "-X", "POST", "-d", "colony=" + colony_id, "-d", "ant=" + ant_id,"ping=" + ping_id, "localhost:5000/hardware"])
+def post_to_server(queen_id, ant_id, ping_id):
+  subprocess.call(["curl", "-X", "POST", "-d", "colony=" + colony_id, "-d", "ant=" + ant_id,"-d", "ping=" + ping_id, "api.olinexpo.com/binds"])
 
 prefix = 'Got payload: '
 while 1:
@@ -16,13 +16,11 @@ while 1:
   if pos != -1:
     message = data[pos + len(prefix):]
     print "message:", message
-    # 8 bytes, each 2 characters big, so make minimum 16 to left pad with appropriate leading 0's
     # these might be reversed
     colony_id = message[0:4]
     ant_id = message[4:12]
     ping_id = message[12:16]
-    print "colony:", colony_id, "ant:", ant_id
-    print "ping_id:", ping_id
-    # post_to_server(colony_id, ant_id);
+    print "colony:", colony_id, "ant:", ant_id, "ping_id:", ping_id
+    post_to_server(colony_id, ant_id, ping_id);
     # subprocess.check_output(["echo", "Hello World!"])
 
